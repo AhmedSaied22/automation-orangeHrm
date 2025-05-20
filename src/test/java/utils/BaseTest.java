867386ecea16bf808942518713a530dd68214c2b
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2025-05-20 19:12:38 +0300

    "finish First task"

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -5,7 +5,6 @@
     import org.junit.Before;
     import org.openqa.selenium.WebDriver;
     import org.openqa.selenium.chrome.ChromeDriver;
-    import org.openqa.selenium.support.ui.WebDriverWait;
     import pages.LoginPage;
 
     import java.time.Duration;
